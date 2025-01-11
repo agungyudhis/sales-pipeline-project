db4cf0616bbc060fb5c4638b88d531fd7f43249e
Yudhistira <96641569+agungyudhis@users.noreply.github.com>
2025-01-11 13:52:40 +0700

    Change update frequecy to every six months

--- dags/dim_date_event_update.py
+++ dags/dim_date_event_update.py
@@ -7,23 +7,28 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.models.taskinstance import TaskInstance
 from airflow.operators.python import PythonOperator
-from connection import create_engine
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from connection import create_engine
+
 
 def _fetch_data(task_instance: TaskInstance | None = None):
-    import requests
     import polars as pl
+    import requests
 
-    response = requests.get(f'https://hari-libur-api.vercel.app/api?year={task_instance.execution_date.year}')
+    response = requests.get(
+        f"https://hari-libur-api.vercel.app/api?year={task_instance.execution_date.year}"
+    )
     data = pl.DataFrame(response.json()).with_columns(
-        pl.col('event_date').map_elements(lambda x: x.replace('-', ''), return_dtype=pl.String),
-        pl.col('is_national_holiday').map_elements(lambda x: "Holiday" if x else "Non-Holiday", return_dtype=pl.String),
+        pl.col("event_date").map_elements(
+            lambda x: x.replace("-", ""), return_dtype=pl.String
+        ),
+        pl.col("is_national_holiday").map_elements(
+            lambda x: "Holiday" if x else "Non-Holiday", return_dtype=pl.String
+        ),
     )
     # Create sqlsalchemy engine to connect the postgres db
-    engine = create_engine(
-        cred_variable="db_credentials", database="warehouse"
-    )
-    data.write_database('staging.stg_events', engine, if_table_exists='replace')
+    engine = create_engine(cred_variable="db_credentials", database="warehouse")
+    data.write_database("staging.stg_events", engine, if_table_exists="replace")
 
 
 default_args = {
@@ -37,10 +42,10 @@ default_args = {
 }
 
 with DAG(
-    "monthly_dim_date_update",
+    "dim_date_update",
     default_args=default_args,
-    description="Monthly fetch national event or holiday data.",
-    schedule_interval="0 0 1 * *",
+    description="Fetch national event or holiday data every six months.",
+    schedule_interval="0 0 1 */6 *",
     start_date=datetime(2022, 1, 1),
     catchup=True,
     max_active_runs=1,
@@ -55,9 +60,7 @@ with DAG(
     )
 
     upsert_data = SQLExecuteQueryOperator(
-        task_id='event_upsert',
-        sql="dim_date_event_update.sql",
-        conn_id="postgres-dw"
+        task_id="event_upsert", sql="dim_date_event_update.sql", conn_id="postgres-dw"
     )
-    
+
     fetch_data >> upsert_data
